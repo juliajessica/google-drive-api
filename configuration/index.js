f719205820a84420d97421e84d28b189a44a1940
julia sheremet <juliajessicasher@gmail.com>
2018-08-12 16:19:20 -0700

    grant drive access to service accounts

--- configuration/index.js
+++ configuration/index.js
@@ -4,8 +4,8 @@ const path = require('path');
 // update this to point to your service account key
 const key = require('../serviceKey.json');
 
-// Target forlder for the Uploaded file. This must be shared with the service account.
-const folderId = "1rpl3pq3P7Nub_wVivNEJFFUSSHsBObUu";
+// Target folder for the Uploaded file. This must be shared with the service account.
+const folderId = "1KYOaHA2PFyrE8GaTWDGMt87ndPFzRwWS";
 
 // location of the file to be uploaded
 const file = '../helloWorld.txt';
